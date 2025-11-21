15faf84d1a29ff25cdad967d7350183906e9a732
vkataguiri <viniciuskataguiri@gmail.com>
2025-11-21 12:22:50 -0300

    feat: app homepage and navigation

--- app/src/main/java/br/ufpr/matemticadivertida/MainActivity.kt
+++ app/src/main/java/br/ufpr/matemticadivertida/MainActivity.kt
@@ -1,20 +1,39 @@
 package br.ufpr.matemticadivertida
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Buttons
+        val btnContagem = findViewById<Button>(R.id.btnContagem)
+        val btnAritmetica = findViewById<Button>(R.id.btnAritmetica)
+        val btnMaiorNumero = findViewById<Button>(R.id.btnMaiorNumero)
+
+        // open counting game
+        btnContagem.setOnClickListener {
+            val intent = Intent(this, ContagemActivity::class.java)
+            startActivity(intent)
+        }
+
+        // open arithmetics game
+        btnAritmetica.setOnClickListener {
+            val intent = Intent(this, AritmeticaActivity::class.java)
+            startActivity(intent)
+        }
+
+        // open largest number game
+        btnMaiorNumero.setOnClickListener {
+            val intent = Intent(this, MaiorNumeroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
